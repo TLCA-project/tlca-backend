5c931a5e60a68dc6c23f4f94ba066bc3f398c8a1
Sébastien Combéfis <sebastien@combefis.be>
2021-12-31 13:06:48 +0100

    Initialise apollo server

---
+++ models/course-model.js
@@ -0,0 +1,202 @@
+const mongoose = require('mongoose');
+
+const CompetencySchema = new mongoose.Schema({
+  competency: {
+    type: Schema.ObjectId,
+    ref: 'Competency'
+  },
+  category: {
+    type: String,
+    enum: ['basic', 'advanced']
+  },
+  subcategory: {
+    type: String,
+    trim: true
+  }
+}, {
+  id: false,
+  _id: false
+});
+
+const GroupSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  supervisor: {
+    type: Schema.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  id: false,
+  _id: false
+});
+
+const ProgressStepSchema = new mongoose.Schema({
+  date: {
+    type: Date,
+    required: true
+  },
+  progress: {
+    basic: {
+      type: Number,
+      min: 0,
+      max: 100,
+      required: true
+    },
+    advanced: {
+      type: Number,
+      min: 0,
+      max: 100,
+      required: true
+    }
+  }
+}, {
+  id: false,
+  _id: false
+});
+
+export const CourseSchema = new mongoose.Schema({
+  code: {
+    type: String,
+    trim: true,
+    required: 'Code cannot be blank.',
+    unique: true
+  },
+  name: {
+    type: String,
+    trim: true,
+    required: 'Name cannot be blank.'
+  },
+  type: {
+    type: String,
+    enum: ['training', 'ucourse'],
+    default: 'training',
+    required: 'Type cannot be blank.'
+  },
+  banner: {
+    type: String
+  },
+  schedule: {
+    start: {
+      type: Date
+    },
+    end: {
+      type: Date
+    },
+    registrationsStart: {
+      type: Date
+    },
+    registrationsEnd: {
+      type: Date
+    },
+    evaluationsEnd: {
+      type: Date
+    }
+  },
+  span: {
+    type: Number,
+    min: 1
+  },
+  load: {
+    type: {
+      type: String,
+      enum: ['weekly', 'theo+prac']
+    },
+    theory: {
+      type: Number,
+      min: 0
+    },
+    practice: {
+      type: Number,
+      min: 0
+    },
+    weekload: {
+      type: Number,
+      min: 1
+    },
+    ects: {
+      type: Number,
+      min: 0
+    }
+  },
+  field: {
+    type: String,
+    trim: true
+  },
+  tags: {
+    type: [{
+      type: String,
+      trim: true
+    }],
+    default: undefined
+  },
+  language: {
+    type: String,
+    trim: true
+  },
+  teachers: {
+    type: [{
+      type: Schema.ObjectId,
+      ref: 'User'
+    }],
+    default: undefined
+  },
+  coordinator: {
+    type: Schema.ObjectId,
+    ref: 'User',
+    required: 'Coordinator cannot be blank.'
+  },
+  partners: {
+    type: [{
+      type: Schema.ObjectId,
+      ref: 'Partner'
+    }],
+    default: undefined
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: 'Description cannot be blank.'
+  },
+  colophon: {
+    type: String,
+    trim: true
+  },
+  competencies: {
+    type: [CompetencySchema],
+    default: undefined
+  },
+  groups: {
+    type: [GroupSchema],
+    default: undefined
+  },
+  progressGuide: {
+    type: [ProgressStepSchema],
+    default: undefined
+  },
+  visibility: {
+    type: String,
+    enum: ['public', 'invite-only', 'private'],
+    default: 'public'
+  },
+  published: {
+    type: Date
+  },
+  archived: {
+    type: Date
+  },
+  clonedFrom: {
+    type: Schema.ObjectId,
+    ref: 'Course'
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  user: {
+    type: Schema.ObjectId,
+    ref: 'User'
+  }
+}, { usePushEach: true });
