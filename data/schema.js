0af8beabce3357044d3e2d3ab1d3ec88f75bc694
Sébastien Combéfis <sebastien@combefis.be>
2022-04-03 19:08:34 +0200

    Update me mutation to check the JWT

--- data/schema.js
+++ data/schema.js
@@ -4,49 +4,49 @@ const typeDefs = gql`
     scalar Date
 
     type Competency {
-        code: String!,
-        name: String!,
+        code: String!
+        name: String!
         description: String
     }
 
     type Event {
-        name: String,
+        name: String
         date: Date
     }
 
     type CourseCompetency {
-        competency: Competency!,
-        category: String!,
+        competency: Competency!
+        category: String!
         subcategory: String
     }
 
     type Course {
-        banner: String,
-        code: String!,
-        colophon: String,
-        competencies: [CourseCompetency!]!,
-        description: String!,
-        field: String,
-        language: String,
-        name: String!,
-        schedule: [Event!],
-        tags: [String!],
+        banner: String
+        code: String!
+        colophon: String
+        competencies: [CourseCompetency!]!
+        description: String!
+        field: String
+        language: String
+        name: String!
+        schedule: [Event!]
+        tags: [String!]
         type: String!
     }
 
     type Program {
-        code: String!,
+        code: String!
         name: String!
     }
 
     type Partner {
-        abbreviation: String,
-        banner: String,
-        code: String!,
-        courses: [Course!],
-        description: String,
-        logo: String,
-        name: String!,
+        abbreviation: String
+        banner: String
+        code: String!
+        courses: [Course!]
+        description: String
+        logo: String
+        name: String!
         website: String
     }
 
@@ -56,21 +56,24 @@ const typeDefs = gql`
 
     type User {
         displayName: String!
+        firstName: String!
+        lastName: String!
+        roles: [String!]!
     }
 
     type Query {
-        courses(offset: Int, limit: Int): [Course!]!,
-        course(code: String!): Course,
-        me: User,
-        partners(limit: Int): [Partner!]!,
-        partner(code: String!): Partner,
-        programs(limit: Int): [Program!]!,
+        courses(offset: Int, limit: Int): [Course!]!
+        course(code: String!): Course
+        me: User
+        partners(limit: Int): [Partner!]!
+        partner(code: String!): Partner
+        programs(limit: Int): [Program!]!
         program(code: String!): Program
     }
 
     type Mutation {
-        signIn(email: String!, password: String!): SignInResponse!,
-        signOut: Boolean,
+        signIn(email: String!, password: String!): SignInResponse!
+        signOut: Boolean
         signUp(firstName: String!, lastName: String!, email: String!, password: String!): Boolean
     }
 `;
