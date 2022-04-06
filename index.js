3c65a2a0bfbf18be60ee5c07e2f7947df55e9a16
Sébastien Combéfis <sebastien@combefis.be>
2022-04-06 13:23:01 +0200

    Set up GraphQL authorisation

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import { ApolloServer } from 'apollo-server';
+import dotenv from 'dotenv'
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { mapSchema, getDirective, MapperKind } from '@graphql-tools/utils';
 import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
+
 import typeDefs from './data/schema.js';
 import resolvers from './data/resolvers.js';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv'
+import { defaultFieldResolver } from 'graphql';
 
 dotenv.config()
 
@@ -19,6 +23,55 @@ db.on('error', () => {
 });
 
 // Configure functions for the GraphQL authentication.
+const authDirective = function(directiveName) {
+  const typeDirectiveArgumentMaps = {};
+  return {
+    authDirectiveTypeDefs: `directive @${directiveName}(
+      requires: [Role]
+    ) on OBJECT | FIELD_DEFINITION
+
+    enum Role {
+        ADMIN
+        MANAGER
+        TEACHER
+        STUDENT
+        USER
+    }`,
+    authDirectiveTransformer: (schema) => {
+      return mapSchema(schema, {
+        [MapperKind.TYPE]: (type) => {
+          const authDirective = getDirective(schema, type, directiveName)?.[0]
+          if (authDirective) {
+            typeDirectiveArgumentMaps[type.name] = authDirective;
+          }
+          return undefined;
+        },
+        [MapperKind.OBJECT_FIELD]: (fieldConfig, _fieldName, typeName) => {
+          const authDirective = getDirective(schema, fieldConfig, directiveName)?.[0] ?? typeDirectiveArgumentMaps[typeName];
+          if (authDirective) {
+            const { resolve = defaultFieldResolver } = fieldConfig;
+            fieldConfig.resolve = function (source, args, context, info) {
+              if (!context.user) {
+                throw new Error('Not authorized');
+              }
+
+              const { requires } = authDirective;
+              if (requires) {
+                if (!requires.some(role => context.user.roles.includes(role.toLowerCase()))) {
+                  throw new Error('Not authorized');
+                }
+              }
+
+              return resolve(source, args, context, info);
+            }
+            return fieldConfig;
+          }
+        }
+      });
+    }
+  };
+};
+
 const getUser = function(token) {
   if (token) {
     try {
@@ -32,9 +85,19 @@ const getUser = function(token) {
 };
 
 // Create and configure the Apollo server.
+const { authDirectiveTypeDefs, authDirectiveTransformer } = authDirective('auth');
+
+let schema = makeExecutableSchema({
+  typeDefs: [
+    authDirectiveTypeDefs,
+    typeDefs
+  ],
+  resolvers
+});
+schema = authDirectiveTransformer(schema);
+
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+  schema,
   context: ({ req }) => {
     const token = req.headers.authorization || '';
     const user = getUser(token);
---
+++ models/registration-model.js
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose';
+import validator from 'validator';
+
+const Schema = mongoose.Schema;
+
+const validateLocalStrategyEmail = function (email) {
+  return validator.isEmail(email, { require_tld: false });
+};
+
+const RegistrationSchema = new Schema({
+  user: {
+    type: Schema.ObjectId,
+    ref: 'User'
+  },
+  course: {
+    type: Schema.ObjectId,
+    ref: 'Course',
+    required: 'Course cannot be blank.'
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  invitation: {
+    type: String,
+    enum: ['requested', 'sent']
+  },
+  email: {
+    type: String,
+    lowercase: true,
+    trim: true,
+    validate: [validateLocalStrategyEmail, 'Please fill a valid email address']
+  },
+  stars: {
+    basic: {
+      type: Number,
+      default: 0
+    },
+    advanced: {
+      type: Number,
+      default: 0
+    }
+  },
+  group: {
+    type: Number
+  }
+});
+
+export default mongoose.model('Registration', RegistrationSchema);
