5e301d6d036046ee01509a789065f06b86a7e1ae
Sébastien Combéfis <sebastien@combefis.be>
2022-06-26 09:32:23 +0200

    Refactor programs related code
    - Fixed code style issues
    - Cleaned program DB model

--- programs/resolvers.js
+++ programs/resolvers.js
@@ -1,59 +1,63 @@
-import { UserInputError } from 'apollo-server';
+import { UserInputError } from 'apollo-server'
 
 const resolvers = {
   ProgramType: {
     TRAINING: 'training',
-    UPROGRAM: 'uprogram'
+    UPROGRAM: 'uprogram',
   },
   Program: {
-    async partners(program, _args, context, _info) {
-      const { Partner } = context.models;
+    async partners(program, _args, { models }, _info) {
+      const { Partner } = models
 
-      const partners = [];
+      const partners = []
 
       if (program.courses) {
-        program.courses.forEach(c => {
-          partners.push(...c.partners);
-        });
+        program.courses.forEach((c) => {
+          partners.push(...c.partners)
+        })
       }
 
-      return await Partner.find({ _id: { $in: partners } });
-    }
+      return await Partner.find({ _id: { $in: partners } })
+    },
   },
   Query: {
-    async programs(_parent, args, context, _info) {
-      const { Program } = context.models;
+    async programs(_parent, args, { models }, _info) {
+      const { Program } = models
 
       // Set up offset and limit.
-      const skip = Math.max(0, args.offset ?? 0);
-      const limit = args.limit ?? undefined;
+      const skip = Math.max(0, args.offset ?? 0)
+      const limit = args.limit ?? undefined
 
       // Retrieve all the courses satisfying the conditions defined hereabove.
-      const programs = await Program.find({}, null, { skip, limit });
+      const programs = await Program.find({}, null, { skip, limit })
 
-      return programs;
+      return programs
     },
-    async program(_parent, args, context, _info) {
-      const { Program } = context.models;
+    async program(_parent, args, { models }, _info) {
+      const { Program } = models
 
-      let program = await Program.findOne({ code: args.code });
+      let program = await Program.findOne({ code: args.code })
       if (!program) {
-        throw new UserInputError('Program not found.');
+        throw new UserInputError('Program not found.')
       }
 
       program = await Program.populate(program, [
-        { path: 'coordinator', select: '_id displayName', model: 'User' },
-      ]).then(c => c.toJSON());
+        { path: 'coordinator', select: '_id displayName username', model: 'User' },
+      ]).then((c) => c.toJSON())
 
-      if (_info.operation.selectionSet.selections[0].selectionSet.selections.find(s => s.name.value === 'courses')) {
+      if (
+        _info.operation.selectionSet.selections[0].selectionSet.selections.find(
+          (s) => s.name.value === 'courses'
+        )
+      ) {
         program = await Program.populate(program, [
           { path: 'courses', model: 'Course' },
-        ]);
+        ])
       }
 
-      return program;
-    }
+      return program
+    },
   },
-};
+}
 
-export default resolvers;
+export default resolvers
