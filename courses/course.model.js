0810987559cb0a847c93647cee69715b48f99cff
Sébastien Combéfis <sebastien@combefis.be>
2022-07-16 21:10:12 +0200

    Update courses resolver
    - Changed from find to aggregate to get the isRegistered derived field
    - Reformated the source code

--- courses/course.model.js
+++ courses/course.model.js
@@ -1,221 +1,242 @@
-import mongoose from 'mongoose';
-import { getPathCompleter } from '../lib/models.js';
+import mongoose from 'mongoose'
+import { getPathCompleter } from '../lib/models.js'
 
-const { Schema } = mongoose;
+const { Schema } = mongoose
 
-const CompetencySchema = new Schema({
-  competency: {
-    type: Schema.ObjectId,
-    ref: 'Competency'
-  },
-  category: {
-    type: String,
-    enum: ['basic', 'advanced']
-  },
-  subcategory: {
-    type: String,
-    trim: true
-  }
-}, {
-  id: false,
-  _id: false
-});
-
-const GroupSchema = new Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  supervisor: {
-    type: Schema.ObjectId,
-    ref: 'User',
-    required: true
-  }
-}, {
-  id: false,
-  _id: false
-});
-
-const ProgressStepSchema = new Schema({
-  date: {
-    type: Date,
-    required: true
-  },
-  progress: {
-    basic: {
-      type: Number,
-      min: 0,
-      max: 100,
-      required: true
+const CompetencySchema = new Schema(
+  {
+    competency: {
+      type: Schema.ObjectId,
+      ref: 'Competency',
     },
-    advanced: {
-      type: Number,
-      min: 0,
-      max: 100,
-      required: true
-    }
+    category: {
+      type: String,
+      enum: ['basic', 'advanced'],
+    },
+    subcategory: {
+      type: String,
+      trim: true,
+    },
+  },
+  {
+    id: false,
+    _id: false,
   }
-}, {
-  id: false,
-  _id: false
-});
+)
 
-const ScheduleSchema = new Schema({
-  start: {
-    type: Date
+const GroupSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    supervisor: {
+      type: Schema.ObjectId,
+      ref: 'User',
+      required: true,
+    },
   },
-  end: {
-    type: Date
-  },
-  registrationsStart: {
-    type: Date
-  },
-  registrationsEnd: {
-    type: Date
-  },
-  evaluationsEnd: {
-    type: Date
+  {
+    id: false,
+    _id: false,
   }
-}, {
-  id: false,
-  _id: false
-});
+)
 
-const CourseSchema = new Schema({
-  code: {
-    type: String,
-    trim: true,
-    required: 'Code cannot be blank.',
-    unique: true
+const ProgressStepSchema = new Schema(
+  {
+    date: {
+      type: Date,
+      required: true,
+    },
+    progress: {
+      basic: {
+        type: Number,
+        min: 0,
+        max: 100,
+        required: true,
+      },
+      advanced: {
+        type: Number,
+        min: 0,
+        max: 100,
+        required: true,
+      },
+    },
   },
-  name: {
-    type: String,
-    trim: true,
-    required: 'Name cannot be blank.'
+  {
+    id: false,
+    _id: false,
+  }
+)
+
+const ScheduleSchema = new Schema(
+  {
+    start: {
+      type: Date,
+    },
+    end: {
+      type: Date,
+    },
+    registrationsStart: {
+      type: Date,
+    },
+    registrationsEnd: {
+      type: Date,
+    },
+    evaluationsEnd: {
+      type: Date,
+    },
   },
-  type: {
-    type: String,
-    enum: ['project', 'training', 'ucourse', 'unit'],
-    default: 'unit',
-    required: 'Type cannot be blank.'
-  },
-  banner: {
-    type: String
-  },
-  schedule: {
-    type: ScheduleSchema,
-    default: undefined
-  },
-  span: {
-    type: Number,
-    min: 1
-  },
-  load: {
+  {
+    id: false,
+    _id: false,
+  }
+)
+
+const CourseSchema = new Schema(
+  {
+    code: {
+      type: String,
+      trim: true,
+      required: 'Code cannot be blank.',
+      unique: true,
+    },
+    name: {
+      type: String,
+      trim: true,
+      required: 'Name cannot be blank.',
+    },
     type: {
       type: String,
-      enum: ['weekly', 'theo+prac']
+      enum: ['project', 'training', 'ucourse', 'unit'],
+      default: 'unit',
+      required: 'Type cannot be blank.',
     },
-    theory: {
-      type: Number,
-      min: 0
-    },
-    practice: {
-      type: Number,
-      min: 0
-    },
-    weekload: {
-      type: Number,
-      min: 1
-    },
-    ects: {
-      type: Number,
-      min: 0
-    }
-  },
-  field: {
-    type: String,
-    trim: true
-  },
-  tags: {
-    type: [{
+    banner: {
       type: String,
-      trim: true
-    }],
-    default: undefined
-  },
-  language: {
-    type: String,
-    trim: true
-  },
-  teachers: {
-    type: [{
+    },
+    schedule: {
+      type: ScheduleSchema,
+      default: undefined,
+    },
+    span: {
+      type: Number,
+      min: 1,
+    },
+    load: {
+      type: {
+        type: String,
+        enum: ['weekly', 'theo+prac'],
+      },
+      theory: {
+        type: Number,
+        min: 0,
+      },
+      practice: {
+        type: Number,
+        min: 0,
+      },
+      weekload: {
+        type: Number,
+        min: 1,
+      },
+      ects: {
+        type: Number,
+        min: 0,
+      },
+    },
+    field: {
+      type: String,
+      trim: true,
+    },
+    tags: {
+      type: [
+        {
+          type: String,
+          trim: true,
+        },
+      ],
+      default: undefined,
+    },
+    language: {
+      type: String,
+      trim: true,
+    },
+    teachers: {
+      type: [
+        {
+          type: Schema.ObjectId,
+          ref: 'User',
+        },
+      ],
+      default: undefined,
+    },
+    coordinator: {
       type: Schema.ObjectId,
-      ref: 'User'
-    }],
-    default: undefined
-  },
-  coordinator: {
-    type: Schema.ObjectId,
-    ref: 'User',
-    required: 'Coordinator cannot be blank.'
-  },
-  partners: {
-    type: [{
+      ref: 'User',
+      required: 'Coordinator cannot be blank.',
+    },
+    partners: {
+      type: [
+        {
+          type: Schema.ObjectId,
+          ref: 'Partner',
+        },
+      ],
+      default: undefined,
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: 'Description cannot be blank.',
+    },
+    colophon: {
+      type: String,
+      trim: true,
+    },
+    competencies: {
+      type: [CompetencySchema],
+      default: undefined,
+    },
+    groups: {
+      type: [GroupSchema],
+      default: undefined,
+    },
+    progressGuide: {
+      type: [ProgressStepSchema],
+      default: undefined,
+    },
+    visibility: {
+      type: String,
+      enum: ['public', 'invite-only', 'private'],
+      default: 'public',
+    },
+    published: {
+      type: Date,
+    },
+    archived: {
+      type: Date,
+    },
+    clonedFrom: {
       type: Schema.ObjectId,
-      ref: 'Partner'
-    }],
-    default: undefined
+      ref: 'Course',
+    },
+    created: {
+      type: Date,
+      default: Date.now,
+    },
+    user: {
+      type: Schema.ObjectId,
+      ref: 'User',
+    },
   },
-  description: {
-    type: String,
-    trim: true,
-    required: 'Description cannot be blank.'
-  },
-  colophon: {
-    type: String,
-    trim: true
-  },
-  competencies: {
-    type: [CompetencySchema],
-    default: undefined
-  },
-  groups: {
-    type: [GroupSchema],
-    default: undefined
-  },
-  progressGuide: {
-    type: [ProgressStepSchema],
-    default: undefined
-  },
-  visibility: {
-    type: String,
-    enum: ['public', 'invite-only', 'private'],
-    default: 'public'
-  },
-  published: {
-    type: Date
-  },
-  archived: {
-    type: Date
-  },
-  clonedFrom: {
-    type: Schema.ObjectId,
-    ref: 'Course'
-  },
-  created: {
-    type: Date,
-    default: Date.now
-  },
-  user: {
-    type: Schema.ObjectId,
-    ref: 'User'
-  }
-}, { usePushEach: true });
+  { usePushEach: true }
+)
 
 // Generate full path for the banner.
-const pathCompleter = getPathCompleter('courses');
-CourseSchema.post('aggregate', pathCompleter);
-CourseSchema.post('find', pathCompleter);
-CourseSchema.post('findOne', pathCompleter);
+const pathCompleter = getPathCompleter('courses')
+CourseSchema.post('aggregate', pathCompleter)
+CourseSchema.post('find', pathCompleter)
+CourseSchema.post('findOne', pathCompleter)
 
-export default mongoose.model('Course', CourseSchema);
+export default mongoose.model('Course', CourseSchema)
