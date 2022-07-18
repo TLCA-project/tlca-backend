45e204c7f6f4fd1553d546005c7edaebccff1dd6
Sébastien Combéfis <sebastien@combefis.be>
2022-07-18 14:10:18 +0200

    Update program type definition and resolver
    - Added field to the Program type and added a few enums
    - Improved the programs resolver and added a few resolvers for program
    fields

--- programs/typeDef.js
+++ programs/typeDef.js
@@ -1,9 +1,22 @@
 import { gql } from 'apollo-server'
 
 const typeDefs = gql`
+  enum ProgramStatus {
+    ARCHIVED
+    PUBLISHED
+    UNPUBLISHED
+  }
+
   enum ProgramType {
     TRAINING
     UPROGRAM
+  }
+
+  enum ProgramView {
+    COORDINATOR
+    STUDENT
+    TEACHER
+    USER
   }
 
   type Program {
@@ -14,19 +27,22 @@ const typeDefs = gql`
     description: String!
     field: String
     hasRequestedInvite: Boolean @auth
+    isArchived: Boolean @auth(requires: [ADMIN, TEACHER])
     isCoordinator: Boolean @auth(requires: TEACHER)
+    isPublished: Boolean @auth(requires: [ADMIN, TEACHER])
     isRegistered: Boolean @auth(requires: STUDENT)
     language: String
     name: String!
     partners: [Partner!]
     registration: Registration @auth
+    status: ProgramStatus @auth(requires: [ADMIN, TEACHER])
     tags: [String!]
     type: ProgramType!
     visibility: Visibility!
   }
 
   extend type Query {
-    programs(offset: Int, limit: Int): [Program!]!
+    programs(offset: Int, limit: Int, view: ProgramView): [Program!]!
     program(code: ID!): Program
   }
 `
