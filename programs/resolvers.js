0b582caf78f546dbe479158f1e04d0df921a45bd
Sébastien Combéfis <sebastien@combefis.be>
2022-07-20 10:47:14 +0200

    Update programs module
    - Added fields and a create mutation in the type definition
    - Created and refactored several resolvers
    - Updated the MongoDB model for programs with new fields

--- programs/resolvers.js
+++ programs/resolvers.js
@@ -1,4 +1,5 @@
 import { UserInputError } from 'apollo-server'
+import mongoose from 'mongoose'
 
 const resolvers = {
   ProgramStatus: {
@@ -10,31 +11,48 @@ const resolvers = {
     TRAINING: 'training',
     UPROGRAM: 'uprogram',
   },
+  ProgramVisibility: {
+    PUBLIC: 'public',
+    INVITE_ONLY: 'invite-only',
+    PRIVATE: 'private',
+  },
   Program: {
+    // Retrieve the detailed information about the coordinator.
     async coordinator(program, _args, { models }, _info) {
       const { User } = models
 
       return await User.findOne({ _id: program.coordinator })
     },
+    // Retrieve whether this program is archived.
     isArchived(program, _args, _context, _info) {
       return !!program.archived
     },
+    // Retrieve whether this program is or has been published.
     isPublished(program, _args, _context, _info) {
       return !!program.published
     },
+    // Retrieve the detailed information about the partners,
+    // that is, the concatenation of all the partners of the
+    // courses associated to this program.
     async partners(program, _args, { models }, _info) {
-      const { Partner } = models
+      const { Course, Partner } = models
 
+      // Retrieve all the courses.
+      const courses = await Course.find(
+        { _id: { $in: program.courses.map((c) => c.course._id || c.course) } },
+        'partners'
+      )
+
+      // Retrieve all the partners.
       const partners = []
-
-      if (program.courses) {
-        program.courses.forEach((c) => {
-          partners.push(...c.partners)
-        })
-      }
+      courses.forEach((c) => {
+        partners.push(...c.partners)
+      })
 
       return await Partner.find({ _id: { $in: partners } })
     },
+    // Retrieve the status of the program according to
+    // it's publication and archive dates.
     status(program, _args, _content, _info) {
       if (!program.published) {
         return 'unpublished'
@@ -50,15 +68,116 @@ const resolvers = {
     },
   },
   Query: {
-    async programs(_parent, args, { models }, _info) {
+    async programs(_parent, args, { models, user }, _info) {
       const { Program } = models
 
-      // Set up offset and limit.
-      const skip = Math.max(0, args.offset ?? 0)
-      const limit = args.limit ?? undefined
+      const pipeline = []
 
-      // Retrieve all the courses satisfying the conditions defined hereabove.
-      const programs = await Program.find({}, null, { skip, limit })
+      // Basically, can only retrieve programs that are:
+      // with the 'public' or invite-only' visibilities,
+      // and with the 'published' status.
+      const filter = {
+        $or: [
+          {
+            $and: [
+              { published: { $exists: true } },
+              { archived: { $exists: false } },
+              {
+                $or: [{ visibility: 'public' }, { visibility: 'invite-only' }],
+              },
+            ],
+          },
+        ],
+      }
+
+      // If a user is connected,
+      // adjust the filter according to his/her roles.
+      if (user && args.view !== 'user') {
+        const { roles } = user
+        const userId = new mongoose.Types.ObjectId(user.id)
+
+        // Teachers can also access their own programs
+        // no matter their status or visibility.
+        if (roles.includes('teacher')) {
+          filter.$or.push({ coordinator: userId })
+        }
+
+        // Students can also access the courses they are registered to
+        // no matter their status or visibility.
+        if (roles.includes('student')) {
+          pipeline.push(
+            {
+              $lookup: {
+                from: 'registrations',
+                localField: '_id',
+                foreignField: 'program',
+                as: 'registrations',
+              },
+            },
+            {
+              $addFields: {
+                isRegistered: {
+                  $anyElementTrue: {
+                    $map: {
+                      input: '$registrations',
+                      as: 'registration',
+                      in: {
+                        $and: [
+                          { $eq: ['$$registration.user', userId] },
+                          {
+                            $eq: [
+                              { $type: '$$registration.invitation' },
+                              'missing',
+                            ],
+                          },
+                        ],
+                      },
+                    },
+                  },
+                },
+              },
+            }
+          )
+
+          filter.$or.push({ isRegistered: true })
+        }
+
+        // Admin can access to all the courses.
+        if (roles.includes('admin')) {
+          delete filter.$or
+        }
+
+        // If the view argument is defined,
+        // adjust the filter according to it.
+        const { view } = args
+        if (view) {
+          switch (view) {
+            case 'coordinator':
+              filter.$or = [{ coordinator: userId }]
+              break
+
+            case 'student':
+              filter.$or = [{ isRegistered: true }]
+              break
+          }
+        }
+      }
+
+      // Only set the defined filter when no user is connected,
+      // or if the connected one has not the admin role.
+      if (!user || !user.roles.includes('admin')) {
+        pipeline.push({ $match: filter })
+      }
+
+      // Set up offset and limit.
+      pipeline.push({ $skip: Math.max(0, args.offset ?? 0) })
+      if (args.limit) {
+        pipeline.push({ $limit: args.limit })
+      }
+
+      // Retrieve all the programs satisfying the conditions defined hereabove
+      pipeline.push({ $project: { registrations: 0 } })
+      const programs = await Program.aggregate(pipeline)
 
       return programs
     },
