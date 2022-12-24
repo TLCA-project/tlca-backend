ca534f4dcddf8cb46dd90a9168e8cf3eb38df001
Sébastien Combéfis <sebastien@combefis.be>
2022-12-24 22:16:24 +0100

    Add evaluation requests end date for courses
    - Updated the course database model with a new evaluationRequestsEnd
    field in the schedule schema
    - Added a constraints check for the schedule field in the pre validation
    function

--- courses/course.model.js
+++ courses/course.model.js
@@ -1,3 +1,4 @@
+import { DateTime } from 'luxon'
 import mongoose from 'mongoose'
 import { getBannerPathCleaner, getPathCompleter } from '../lib/models.js'
 
@@ -59,7 +60,10 @@ const ScheduleSchema = new Schema(
     end: {
       type: Date,
     },
-    start: {
+    evaluationRequestsEnd: {
+      type: Date,
+    },
+    evaluationsEnd: {
       type: Date,
     },
     registrationsEnd: {
@@ -68,7 +72,7 @@ const ScheduleSchema = new Schema(
     registrationsStart: {
       type: Date,
     },
-    evaluationsEnd: {
+    start: {
       type: Date,
     },
   },
@@ -258,6 +262,159 @@ const CourseSchema = new Schema(
   { usePushEach: true }
 )
 
+function checkSchedule(context) {
+  const getDate = (event) =>
+    context.schedule[event]
+      ? DateTime.fromJSDate(context.schedule[event])
+      : undefined
+  const invalidate = (event, key) =>
+    context.invalidate(`schedule-${event}`, key)
+
+  const end = getDate('end')
+  const evaluationRequestsEnd = getDate('evaluationRequestsEnd')
+  const evaluationsEnd = getDate('evaluationsEnd')
+  const registrationsEnd = getDate('registrationsEnd')
+  const registrationsStart = getDate('registrationsStart')
+  const start = getDate('start')
+
+  // Registration start date should be...
+  if (registrationsStart) {
+    // ...before registration end date.
+    if (registrationsEnd && registrationsStart >= registrationsEnd) {
+      invalidate(
+        'registrationsStart',
+        'REGISTRATIONS_START_SAME_OR_AFTER_REGISTRATIONS_END_DATE'
+      )
+      invalidate(
+        'registrationsEnd',
+        'REGISTRATIONS_END_SAME_OR_BEFORE_REGISTRATIONS_START_DATE'
+      )
+    }
+
+    // ...before or equal to course start date.
+    if (start && registrationsStart > start) {
+      invalidate('registrationsStart', 'REGISTRATIONS_START_AFTER_START_DATE')
+      invalidate('start', 'START_BEFORE_REGISTRATIONS_START_DATE')
+    }
+
+    // ...before course end date.
+    if (end && registrationsStart >= end) {
+      invalidate(
+        'registrationsStart',
+        'REGISTRATIONS_START_SAME_OR_AFTER_END_DATE'
+      )
+      invalidate('end', 'END_SAME_OR_BEFORE_REGISTRATIONS_START_DATE')
+    }
+
+    // ...before evaluation requests end date.
+    if (evaluationRequestsEnd && registrationsStart >= evaluationRequestsEnd) {
+      invalidate(
+        'registrationsStart',
+        'REGISTRATIONS_START_SAME_OR_AFTER_EVALUATION_REQUESTS_END_DATE'
+      )
+      invalidate(
+        'evaluationRequestsEnd',
+        'EVALUATION_REQUESTS_END_SAME_OR_BEFORE_REGISTRATIONS_START_DATE'
+      )
+    }
+
+    // ...before evaluations end date.
+    if (evaluationsEnd && registrationsStart >= evaluationsEnd) {
+      invalidate(
+        'registrationsStart',
+        'REGISTRATIONS_START_SAME_OR_AFTER_EVALUATIONS_END_DATE'
+      )
+      invalidate(
+        'evaluationsEnd',
+        'EVALUATIONS_END_SAME_OR_BEFORE_REGISTRATIONS_START_DATE'
+      )
+    }
+  }
+
+  // Registration end date should be...
+  if (registrationsEnd) {
+    // ...before course end date (if there are no evaluations end)..
+    if (!evaluationsEnd && end && registrationsEnd >= end) {
+      invalidate('registrationsEnd', 'REGISTRATIONS_END_SAME_OR_AFTER_END_DATE')
+      invalidate('end', 'END_SAME_OR_BEFORE_REGISTRATIONS_END_DATE')
+    }
+
+    // ...before evaluations end date.
+    if (evaluationsEnd && registrationsEnd >= evaluationsEnd) {
+      invalidate(
+        'registrationsEnd',
+        'REGISTRATIONS_END_SAME_OR_AFTER_EVALUATIONS_END_DATE'
+      )
+      invalidate(
+        'evaluationsEnd',
+        'EVALUATIONS_END_SAME_OR_BEFORE_REGISTRATIONS_END_DATE'
+      )
+    }
+  }
+
+  // Course start date should be...
+  if (start) {
+    // ...before course end date.
+    if (end && start >= end) {
+      invalidate('start', 'START_SAME_OR_AFTER_END_DATE')
+      invalidate('end', 'END_SAME_OR_BEFORE_START_DATE')
+    }
+
+    // ...before evaluation requests end date.
+    if (evaluationRequestsEnd && start >= evaluationRequestsEnd) {
+      invalidate('start', 'START_SAME_OR_AFTER_EVALUATION_REQUESTS_END_DATE')
+      invalidate(
+        'evaluationRequestsEnd',
+        'EVALUATION_REQUESTS_END_SAME_OR_BEFORE_START_DATE'
+      )
+    }
+
+    // ...before evaluations end date.
+    if (evaluationsEnd && start >= evaluationsEnd) {
+      invalidate('start', 'START_SAME_OR_AFTER_EVALUATIONS_END_DATE')
+      invalidate('evaluationsEnd', 'EVALUATIONS_END_SAME_OR_BEFORE_START_DATE')
+    }
+  }
+
+  // Course end date should be...
+  if (end) {
+    // ...after evaluations requests end date (if there are no evaluations end).
+    if (
+      !evaluationsEnd &&
+      evaluationRequestsEnd &&
+      end <= evaluationRequestsEnd
+    ) {
+      invalidate('end', 'END_SAME_OR_BEFORE_EVALUATION_REQUESTS_END_DATE')
+      invalidate(
+        'evaluationRequestsEnd',
+        'EVALUATION_REQUESTS_END_SAME_OR_AFTER_END_DATE'
+      )
+    }
+
+    // ...before or equal to evaluations end date.
+    if (evaluationsEnd && end >= evaluationsEnd) {
+      invalidate('end', 'END_SAME_OR_AFTER_EVALUATIONS_END_DATE')
+      invalidate('evaluationsEnd', 'EVALUATIONS_END_SAME_OR_BEFORE_END_DATE')
+    }
+  }
+
+  // Evaluation requests end date should be before evaluations end date.
+  if (
+    evaluationRequestsEnd &&
+    evaluationsEnd &&
+    evaluationRequestsEnd >= evaluationsEnd
+  ) {
+    invalidate(
+      'evaluationRequestsEnd',
+      'EVALUATION_REQUESTS_END_SAME_OR_AFTER_EVALUATIONS_END_DATE'
+    )
+    invalidate(
+      'evaluationsEnd',
+      'EVALUATIONS_END_SAME_OR_BEFORE_EVALUATION_REQUESTS_END_DATE'
+    )
+  }
+}
+
 CourseSchema.pre('validate', function (next) {
   // There must be at least one basic competency.
   if (!this.competencies.some((c) => c.category === 'basic')) {
@@ -274,6 +431,11 @@ CourseSchema.pre('validate', function (next) {
     )
   ) {
     this.invalidate('competencies', 'DUPLICATE_COMPETENCIES')
+  }
+
+  // Schedule must be coherent.
+  if (this.schedule) {
+    checkSchedule(this)
   }
 
   // Teaching group names must be all different.
